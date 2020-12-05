acd8c71d725d605f814af9b5747b390640c5deb4
Clayton Mork <mork.clayton3@gmail.com>
2020-12-05 21:00:12 +0900

    fix

--- gan_utils/lib/train.py
+++ gan_utils/lib/train.py
@@ -157,7 +157,7 @@ class Trainer:
                         fake = self.netG(self.fixed_noise).detach().cpu()
                     img_grid = vutils.make_grid(fake, padding=2, normalize=True)
                     img_grid = img_grid.numpy().transpose((1,2,0)) * 255
-                    img_grid = cv2.cvtColor(img_grid, cv2.COLOR_RGB2BGR)
+                    img_grid = cv2.cvtColor(img_grid, cv2.COLOR_RGB2BGR).astype('uint8')
                     cv2.imwrite('vis.png', img_grid)
 
                     if self.video_save is not None:
